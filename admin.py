214996b2268e39cf5f36e27a9f00909947b5c27f
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2025-04-23 10:34:08 -0400

    chagned report

--- admin.py
+++ admin.py
@@ -53,7 +53,7 @@ tabs = st.tabs([
     "Catalog",
 ])
 
-# — Browse Drive Tab —
+# — Inspect PDFs Tab —
 with tabs[0]:
     pdf_container = st.empty()
 
@@ -108,10 +108,9 @@ with tabs[3]:
         st.markdown(f"{num_items} items. Last update: {last_update_date}")  
 
         # Display the DataFrame
-        display_df = df[['title', 'publication_number', 'organization', 'issue_date', 'expiration_date', 'scope', 'unit']]
-        edited_df = st.data_editor(display_df, use_container_width=True, hide_index=False, disabled=True)
+        st.data_editor(df, use_container_width=True, hide_index=False, disabled=True)
         isim = f'ASK_catalog_export{last_update_date}.csv'
-        indir = edited_df.to_csv(index=False)
+        indir = df.to_csv(index=False)
         b64 = base64.b64encode(indir.encode(encoding='utf-8')).decode(encoding='utf-8')  
         linko_final = f'<a href="data:file/csv;base64,{b64}" download="{isim}">Click to download the catalog</a>'
         st.markdown(linko_final, unsafe_allow_html=True)
