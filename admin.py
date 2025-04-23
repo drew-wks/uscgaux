c765d05063c32af8529d3a31a23ea27cd44ccf56
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2025-04-22 22:16:01 -0400

    Update admin.py

--- admin.py
+++ admin.py
@@ -13,7 +13,7 @@ sheets, drive = get_gcp_clients()
 # ——————————————————————————————
 # 3) Load the catalog DataFrame once
 LIBRARY_CATALOG_ID = "16F5tRIvuHncofRuXCsQ20A7utZWRuEgA2bvj4nQQjek"
-sheet = sheets.open(LIBRARY_CATALOG_ID).sheet1
+sheet = sheets.open_by_key(LIBRARY_CATALOG_ID).sheet1
 records = sheet.get_all_records()
 catalog_df = pd.DataFrame(records)
 
