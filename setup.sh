02afadfcb9e1ff125d8986a381b277f8deb5f57f
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2025-06-11 22:41:21 -0400

    added newlines to requirements file

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+# Codex-compatible setup script
+
+echo "Installing main dependencies..."
 pip install -r requirements.txt
+
+echo "Installing dev/test dependencies..."
+pip install -r requirements-dev.txt
